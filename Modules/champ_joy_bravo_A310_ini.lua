819156e00e780db9620383f63fdd56c0378fb9b4
Cedrik Lussier <lussier.cedrik@gmail.com>
2021-08-06 18:52:52 -0400

    A300 A310 ini: Finish mapping

--- Modules/champ_joy_bravo_A310_ini.lua
+++ Modules/champ_joy_bravo_A310_ini.lua
@@ -60,6 +60,8 @@ function ChampBravoMapping_A310_ini()
 
   do_every_frame("apPanelDualHandler()")
 
+  set_button_assignment(btq.axis3_toga, "A300/MCDU/takeoff_goaround_trigger")
+
   set_button_assignment(btq.sw2_up, "sim/none/none")
   set_button_assignment(btq.sw2_dn, "sim/none/none")
   do_every_frame([[
@@ -185,6 +187,76 @@ function ChampBravoLed_A310_ini()
     (
       (get("A300/MCDU/animations/lights/autothrottle") == 1)
     ), btq_led, 'A', btq_led.block_A_LED.IAS)
+
+  btq_led.led_check(
+    (
+      (get("A300/master_warning") == 1)
+    ), btq_led, 'B', btq_led.block_B_LED.MASTER_WARNING)
+  btq_led.led_check(
+    (
+      (get("A300/master_caution_light") == 1)
+    ), btq_led, 'C', btq_led.block_C_LED.MASTER_CAUTION)
+  btq_led.led_check(
+    (
+      (get("A300/FIRE/fire_handle_engine1_light") == 1) or
+      (get("A300/FIRE/fire_handle_apu_light")     == 1) or
+      (get("A300/FIRE/fire_handle_engine2_light") == 1)
+    ), btq_led, 'B', btq_led.block_B_LED.ENGINE_FIRE)
+  btq_led.led_check(
+    (
+      (get("A300/engine/engine1_oil_low_press_light") == 1) or
+      (get("A300/engine/engine2_oil_low_press_light") == 1)
+    ), btq_led, 'C', btq_led.block_C_LED.LOW_OIL_PRESS)
+  btq_led.led_check(
+    (
+      (get("A300/HYD/hydraulic_blue_light")   == 1) or
+      (get("A300/HYD/hydraulic_green1_light") == 1) or
+      (get("A300/HYD/hydraulic_green2_light") == 1) or
+      (get("A300/HYD/hydraulic_green3_light") == 1) or
+      (get("A300/HYD/hydraulic_yellow_light") == 1)
+    ), btq_led, 'C', btq_led.block_C_LED.LOW_HYD_PRESS)
+  btq_led.led_check(
+    (
+      (get("A300/FUEL/outer_tank_pump1_left_low_pressure")  == 1 ) or
+      (get("A300/FUEL/outer_tank_pump2_left_low_pressure")  == 1 ) or
+      (get("A300/FUEL/outer_tank_pump1_right_low_pressure") == 1 ) or
+      (get("A300/FUEL/outer_tank_pump2_right_low_pressure") == 1 ) or
+      (get("A300/FUEL/center_tank_pump1_low_pressure")      == 1 ) or
+      (get("A300/FUEL/center_tank_pump2_low_pressure")      == 1 ) or
+      (get("A300/FUEL/inner_tank_pump1_right_low_pressure") == 1 ) or
+      (get("A300/FUEL/inner_tank_pump2_right_low_pressure") == 1 ) or
+      (get("A300/FUEL/inner_tank_pump2_left_low_pressure")  == 1 ) or
+      (get("A300/FUEL/inner_tank_pump1_left_low_pressure")  == 1 ) or
+      (get("A300/FUEL/trim_tank_pump2_low_pressure")        == 1 ) or
+      (get("A300/FUEL/trim_tank_pump1_low_pressure")        == 1 )
+    ), btq_led, 'C', btq_led.block_C_LED.LOW_FUEL_PRESS)
+  btq_led.led_check(
+    (
+      (get("sim/cockpit2/ice/cowling_thermal_anti_ice_per_engine", 0) == 1) or
+      (get("sim/cockpit2/ice/cowling_thermal_anti_ice_per_engine", 1) == 1) or
+      (get("A300/ICE/wing_supply") == 1)
+    ), btq_led, 'C', btq_led.block_C_LED.ANTI_ICE)
+
+  btq_led.led_check(
+    (
+      (get("A300/APU/master_switch_button") == 1) or
+      (get("A300/APU/n1") > 5.0)
+    ), btq_led, 'C', btq_led.block_C_LED.APU)
+  btq_led.led_check(
+    (
+      (get("A300/GND/doors_ratio", 0) > 0.1 ) or
+      (get("A300/GND/doors_ratio", 1) > 0.1 ) or
+      (get("A300/GND/doors_ratio", 2) > 0.1 ) or
+      (get("A300/GND/doors_ratio", 3) > 0.1 ) or
+      (get("A300/GND/doors_ratio", 4) > 0.1 ) or
+      (get("A300/GND/doors_ratio", 5) > 0.1 ) or
+      (get("A300/GND/doors_ratio", 6) > 0.1 ) or
+      (get("A300/GND/doors_ratio", 7) > 0.1 ) or
+      (get("A300/GND/doors_ratio", 8) > 0.1 ) or
+      (get("A300/GND/doors_ratio", 9) > 0.1 ) 
+    ), btq_led, 'D', btq_led.block_D_LED.DOOR)
+
+
   if (
       (get("A300/elec/battery_on")      == 0) and
       (get("A300/elec/dc_norm_bus_off") == 1) and
@@ -196,62 +268,92 @@ end
 
 
 function ChampBravoCheck_A310_ini()
-  if (XPLMFindDataRef("A300/elec/battery_on")                               ~= nil and
-      XPLMFindDataRef("A300/elec/dc_norm_bus_off")                          ~= nil and
-      XPLMFindDataRef("A300/elec/ac_bus1_off")                              ~= nil and
-      XPLMFindDataRef("A300/elec/ac_bus2_off")                              ~= nil and
+  if (XPLMFindDataRef("A300/elec/battery_on")                                 ~= nil and
+      XPLMFindDataRef("A300/elec/dc_norm_bus_off")                            ~= nil and
+      XPLMFindDataRef("A300/elec/ac_bus1_off")                                ~= nil and
+      XPLMFindDataRef("A300/elec/ac_bus2_off")                                ~= nil and
 
       --AP Panel
-      XPLMFindCommand("A300/MCDU/altitude_up")                              ~= nil and
-      XPLMFindCommand("A300/MCDU/altitude_down")                            ~= nil and
-      XPLMFindCommand("A300/MCDU/vvi_up")                                   ~= nil and
-      XPLMFindCommand("A300/MCDU/vvi_down")                                 ~= nil and
-      XPLMFindCommand("A300/MCDU/heading_up")                               ~= nil and
-      XPLMFindCommand("A300/MCDU/heading_down")                             ~= nil and
-      XPLMFindCommand("A300/radios/ILS/course/dial_up")                     ~= nil and
-      XPLMFindCommand("A300/radios/ILS/course/dial_down")                   ~= nil and
-      XPLMFindCommand("A300/MCDU/airspeed_up")                              ~= nil and
-      XPLMFindCommand("A300/MCDU/airspeed_down")                            ~= nil and
+      XPLMFindCommand("A300/MCDU/altitude_up")                                ~= nil and
+      XPLMFindCommand("A300/MCDU/altitude_down")                              ~= nil and
+      XPLMFindCommand("A300/MCDU/vvi_up")                                     ~= nil and
+      XPLMFindCommand("A300/MCDU/vvi_down")                                   ~= nil and
+      XPLMFindCommand("A300/MCDU/heading_up")                                 ~= nil and
+      XPLMFindCommand("A300/MCDU/heading_down")                               ~= nil and
+      XPLMFindCommand("A300/radios/ILS/course/dial_up")                       ~= nil and
+      XPLMFindCommand("A300/radios/ILS/course/dial_down")                     ~= nil and
+      XPLMFindCommand("A300/MCDU/airspeed_up")                                ~= nil and
+      XPLMFindCommand("A300/MCDU/airspeed_down")                              ~= nil and
 
-      XPLMFindCommand("A300/MCDU/heading_pull")                             ~= nil and
-      XPLMFindCommand("A300/MCDU/nav_mode")                                 ~= nil and
-      XPLMFindCommand("A300/MCDU/loc_toggle")                               ~= nil and
-      XPLMFindCommand("A300/MCDU/profile_mode")                             ~= nil and
-      XPLMFindCommand("A300/MCDU/approach_mode")                            ~= nil and
-      XPLMFindCommand("A300/MCDU/level_change")                             ~= nil and
-      XPLMFindCommand("A300/MCDU/altitude_hold_engage")                     ~= nil and
-      XPLMFindCommand("A300/MCDU/vvi_engage")                               ~= nil and
-      XPLMFindCommand("A300/MCDU/ap1_engage")                               ~= nil and
-      XPLMFindCommand("A300/MCDU/autothrottle_toggle")                      ~= nil and
+      XPLMFindCommand("A300/MCDU/heading_pull")                               ~= nil and
+      XPLMFindCommand("A300/MCDU/nav_mode")                                   ~= nil and
+      XPLMFindCommand("A300/MCDU/loc_toggle")                                 ~= nil and
+      XPLMFindCommand("A300/MCDU/profile_mode")                               ~= nil and
+      XPLMFindCommand("A300/MCDU/approach_mode")                              ~= nil and
+      XPLMFindCommand("A300/MCDU/level_change")                               ~= nil and
+      XPLMFindCommand("A300/MCDU/altitude_hold_engage")                       ~= nil and
+      XPLMFindCommand("A300/MCDU/vvi_engage")                                 ~= nil and
+      XPLMFindCommand("A300/MCDU/ap1_engage")                                 ~= nil and
+      XPLMFindCommand("A300/MCDU/autothrottle_toggle")                        ~= nil and
 
       --Axis
+      XPLMFindCommand("A300/MCDU/takeoff_goaround_trigger")                   ~= nil and
 
       --Switches
-      XPLMFindDataRef("A300/animations/buttons/ANTI_ICE_E1")                ~= nil and
-      XPLMFindDataRef("A300/animations/buttons/ANTI_ICE_E2")                ~= nil and
-      XPLMFindCommand("A300/ICE/eng1_toggle")                               ~= nil and
-      XPLMFindCommand("A300/ICE/eng2_toggle")                               ~= nil and
-      XPLMFindDataRef("A300/animations/buttons/WING_SUPPLY")                ~= nil and
-      XPLMFindCommand("A300/ICE/wing_supply_toggle")                        ~= nil and
-      XPLMFindDataRef("A300/switches/animations_current")                   ~= nil and
-      XPLMFindCommand("A300/LIGHTS/strobe_light_up")                        ~= nil and
-      XPLMFindCommand("A300/LIGHTS/strobe_light_down")                      ~= nil and
-      XPLMFindCommand("A300/LIGHTS/landing_light1_up")                      ~= nil and
-      XPLMFindCommand("A300/LIGHTS/landing_light2_up")                      ~= nil and
-      XPLMFindCommand("A300/LIGHTS/landing_light1_down")                    ~= nil and
-      XPLMFindCommand("A300/LIGHTS/landing_light2_down")                    ~= nil and
-      XPLMFindCommand("A300/LIGHTS/nose_light_up")                          ~= nil and
-      XPLMFindCommand("A300/LIGHTS/nose_light_down")                        ~= nil and
+      XPLMFindDataRef("A300/animations/buttons/ANTI_ICE_E1")                  ~= nil and
+      XPLMFindDataRef("A300/animations/buttons/ANTI_ICE_E2")                  ~= nil and
+      XPLMFindCommand("A300/ICE/eng1_toggle")                                 ~= nil and
+      XPLMFindCommand("A300/ICE/eng2_toggle")                                 ~= nil and
+      XPLMFindDataRef("A300/animations/buttons/WING_SUPPLY")                  ~= nil and
+      XPLMFindCommand("A300/ICE/wing_supply_toggle")                          ~= nil and
+      XPLMFindDataRef("A300/switches/animations_current")                     ~= nil and
+      XPLMFindCommand("A300/LIGHTS/strobe_light_up")                          ~= nil and
+      XPLMFindCommand("A300/LIGHTS/strobe_light_down")                        ~= nil and
+      XPLMFindCommand("A300/LIGHTS/landing_light1_up")                        ~= nil and
+      XPLMFindCommand("A300/LIGHTS/landing_light2_up")                        ~= nil and
+      XPLMFindCommand("A300/LIGHTS/landing_light1_down")                      ~= nil and
+      XPLMFindCommand("A300/LIGHTS/landing_light2_down")                      ~= nil and
+      XPLMFindCommand("A300/LIGHTS/nose_light_up")                            ~= nil and
+      XPLMFindCommand("A300/LIGHTS/nose_light_down")                          ~= nil and
 
       --LEDs
-      XPLMFindDataRef("A300/MCDU/animations/lights/heading_sel")            ~= nil and
-      XPLMFindDataRef("A300/MCDU/animations/lights/nav")                    ~= nil and
-      XPLMFindDataRef("A300/MCDU/animations/lights/profile_mode")           ~= nil and
-      XPLMFindDataRef("A300/MCDU/animations/lights/approach")               ~= nil and
-      XPLMFindDataRef("A300/MCDU/animations/lights/level_change")           ~= nil and
-      XPLMFindDataRef("A300/MCDU/animations/lights/altitude_hold")          ~= nil and
-      --XPLMFindDataRef("a320/Aircraft/Pneumatic/Cabin/CargoClosedB")                  ~= nil
-      true
+      XPLMFindDataRef("A300/MCDU/animations/lights/heading_sel")              ~= nil and
+      XPLMFindDataRef("A300/MCDU/animations/lights/nav")                      ~= nil and
+      XPLMFindDataRef("A300/MCDU/animations/lights/profile_mode")             ~= nil and
+      XPLMFindDataRef("A300/MCDU/animations/lights/approach")                 ~= nil and
+      XPLMFindDataRef("A300/MCDU/animations/lights/level_change")             ~= nil and
+      XPLMFindDataRef("A300/MCDU/animations/lights/altitude_hold")            ~= nil and
+
+      XPLMFindDataRef("A300/master_warning")                                  ~= nil and
+      XPLMFindDataRef("A300/master_caution_light")                            ~= nil and
+      XPLMFindDataRef("A300/FIRE/fire_handle_engine1_light")                  ~= nil and
+      XPLMFindDataRef("A300/FIRE/fire_handle_apu_light")                      ~= nil and
+      XPLMFindDataRef("A300/FIRE/fire_handle_engine2_light")                  ~= nil and
+      XPLMFindDataRef("A300/engine/engine1_oil_low_press_light")              ~= nil and
+      XPLMFindDataRef("A300/engine/engine2_oil_low_press_light")              ~= nil and
+      XPLMFindDataRef("A300/HYD/hydraulic_blue_light")                        ~= nil and
+      XPLMFindDataRef("A300/HYD/hydraulic_green1_light")                      ~= nil and
+      XPLMFindDataRef("A300/HYD/hydraulic_green2_light")                      ~= nil and
+      XPLMFindDataRef("A300/HYD/hydraulic_green3_light")                      ~= nil and
+      XPLMFindDataRef("A300/HYD/hydraulic_yellow_light")                      ~= nil and
+      XPLMFindDataRef("A300/FUEL/outer_tank_pump1_left_low_pressure")         ~= nil and
+      XPLMFindDataRef("A300/FUEL/outer_tank_pump2_left_low_pressure")         ~= nil and
+      XPLMFindDataRef("A300/FUEL/outer_tank_pump1_right_low_pressure")        ~= nil and
+      XPLMFindDataRef("A300/FUEL/outer_tank_pump2_right_low_pressure")        ~= nil and
+      XPLMFindDataRef("A300/FUEL/center_tank_pump1_low_pressure")             ~= nil and
+      XPLMFindDataRef("A300/FUEL/center_tank_pump2_low_pressure")             ~= nil and
+      XPLMFindDataRef("A300/FUEL/inner_tank_pump1_right_low_pressure")        ~= nil and
+      XPLMFindDataRef("A300/FUEL/inner_tank_pump2_right_low_pressure")        ~= nil and
+      XPLMFindDataRef("A300/FUEL/inner_tank_pump2_left_low_pressure")         ~= nil and
+      XPLMFindDataRef("A300/FUEL/inner_tank_pump1_left_low_pressure")         ~= nil and
+      XPLMFindDataRef("A300/FUEL/trim_tank_pump2_low_pressure")               ~= nil and
+      XPLMFindDataRef("A300/FUEL/trim_tank_pump1_low_pressure")               ~= nil and
+      XPLMFindDataRef("sim/cockpit2/ice/cowling_thermal_anti_ice_per_engine") ~= nil and
+      XPLMFindDataRef("A300/ICE/wing_supply")                                 ~= nil and
+      XPLMFindDataRef("A300/APU/master_switch_button")                        ~= nil and
+      XPLMFindDataRef("A300/APU/n1")                                          ~= nil and
+      XPLMFindDataRef("A300/GND/doors_ratio")                                 ~= nil
+
      ) then return true
   else
     return false
